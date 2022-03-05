71adce2dc8198b2b95e7a9ea3b59e3b3d7cadd3a
smarcosm <sebastiaomarcos49@gmail.com>
2022-03-05 18:00:35 -0300

    Effect, profile picture and button

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 export default function Profile(){
   return (
@@ -21,6 +22,48 @@ export default function Profile(){
                 <a href="#">
                   <i className="fa fa-twitter"></i> 
                 </a>              
+            </div>
+
+            <div className="profile-detals-name">
+              <span className="primary-text">
+                {" "}
+                Olá, eu sou <span className="highkighted-text">Sebastião</span>
+              </span>
+            </div>
+            <div className="profile-detals-role">
+              <span className="primary-text">
+                {" "}
+                <h1>
+                  {" "}
+                    <Typical
+                      loop={Infinity}
+                      steps={[
+                        "Ethusiastic dev 🔴",
+                        1500,
+                        "Full Stack Developer 💻",
+                        1500,
+                        "MERN Stack dev 😎",
+                        1500,
+                        "Cross Platform dev 🌐",
+                        1500,
+                        "React dev 🌐",
+                        1500,
+                      ]}
+                    />
+                </h1>
+                <span className="profile-role-tagline">
+                      Gosto de construir aplicações backend e frontend.
+                </span>
+              </span>
+            </div>
+            <div className="profile-options">
+              <button className="btn primary-btn">
+                {""}
+                Contrate-me{" "}
+              </button>
+              <a href="cv.pdf" download="Sebastião cv.pdf">
+                <button className="btn highlighted-btn">Pega Resumo</button>
+              </a>
             </div>
           </div>
         </div>
